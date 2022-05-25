8474c7f839c0fc7ae774c88bff6d2549015e4726
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2022-05-25 10:45:02 +0200

    Auth + API

---
+++ bot.py
@@ -0,0 +1,10 @@
+import tweepy #Python Library for Twitter
+
+def auth(conskey,conssecret,acctoken,acctokensecret): #Authentification
+    auth = tweepy.OAuthHandler(conskey,conssecret)
+    auth.set_access_token(acctoken,acctokensecret)
+    return auth
+
+def createapi(auth): #Create API Obj
+    api = tweepy.api(auth)
+    return api
